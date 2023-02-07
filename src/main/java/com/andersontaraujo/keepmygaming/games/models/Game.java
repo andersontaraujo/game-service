bb99d49acd1f0dd9d4767812b0cbf6787d7102bb
Anderson Araújo <aaraujo.solks@digio.com.br>
2023-02-07 01:56:23 -0300

    added auditing fields

--- src/main/java/com/andersontaraujo/keepmygaming/games/models/Game.java
+++ src/main/java/com/andersontaraujo/keepmygaming/games/models/Game.java
@@ -1,9 +1,12 @@
 package com.andersontaraujo.keepmygaming.games.models;
 
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -16,4 +19,8 @@ public class Game {
     private String developerName;
     private Integer yearOfRelease;
     private List<Platform> platforms;
+    @CreatedDate
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
 }
